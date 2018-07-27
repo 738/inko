023374efb4f56f9099504e397ea00fc37d3e14ed
junwooji <junwoo@finda.co.kr>
2018-07-28 00:12:59 +0900

    모음 처리하는 부분 수정

--- en2ko.js
+++ en2ko.js
@@ -5,7 +5,6 @@ const 중성 = "ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ"
 const 종성 = "ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ";
 const 첫모음 = 19;
 
-
 class Enko {
     en2ko(input) {
         let result = '';
@@ -36,10 +35,10 @@ class Enko {
                 result += char;
             }
             // 자음이라면
-            else if (index < 19) {
+            else if (index < 첫모음) {
                 if (_중성 !== -1) {
                     // 중성만 입력됨
-                    if (_초성 !== -1) {
+                    if (_초성 === -1) {
                         result += 중성[_중성];
                         _중성 = -1;
                         _초성 = 초성.indexOf(_한글);
@@ -80,11 +79,11 @@ class Enko {
                     }
                 } else {
                     if (_초성 === -1) {
-                        // if (_종성 !== -1) {				// 복자음 후 초성
-                        //     result += 종성[_종성];
-                        //     _종성 = -1;
-                        // }
-                        // _초성 = 초성.indexOf(_한글);
+                        if (_종성 !== -1) {				// 복자음 후 초성
+                            result += 종성[_종성];
+                            _종성 = -1;
+                        }
+                        _초성 = 초성.indexOf(_한글);
                     }
                     // 복자음 처리
                     else if (_초성 === 초성.indexOf('ㄱ') && _한글 === 'ㅅ') _종성 = 종성.indexOf('ㄳ'), _초성 = -1;   // 복자음 ㄳ
@@ -112,39 +111,89 @@ class Enko {
             }
             // 모음이라면
             else {
-                // 초성이 없다면 모음이 하나 왔다는 의미
-                if (_초성 === -1) _중성 = 중성.indexOf(_한글);
-                // 초성이 있고 중성이 없다면
-                else if (_초성 !== -1 && _중성 === -1) _중성 = 중성.indexOf(_한글);
-                // 초성이 있고 중성도 있다면 (복모음)
-                else if (_초성 !== -1 && _중성 !== -1) {
-                    if (_중성 === 중성.indexOf('ㅗ')) {
-                        if (index === 한글.indexOf('ㅏ')) _중성 = 중성.indexOf('ㅘ');                               // 복모음 ㅘ
-                        else if (index === 한글.indexOf('ㅐ')) _중성 = 중성.indexOf('ㅙ');                          // 복모음 ㅙ
-                        else if (index === 한글.indexOf('ㅣ')) _중성 = 중성.indexOf('ㅚ');                          // 복모음 ㅚ
+                if (_종성 !== -1) {						// (앞글자 종성), 초성+중성
+                    // 복자음 다시 분해
+                    var 새초성;			                               // 임시 초성
+                    if (_종성 === 종성.indexOf('ㄳ')) {					// ㄱ / ㅅ
+                        _종성 = 종성.indexOf('ㄱ');
+                        새초성 = 초성.indexOf('ㅅ');
+                    } else if (_종성 === 종성.indexOf('ㄵ')) {			// ㄴ / ㅈ
+                        _종성 = 종성.indexOf('ㄴ');
+                        새초성 = 초성.indexOf('ㅈ');
+                    } else if (_종성 === 종성.indexOf('ㄶ')) {			// ㄴ / ㅎ
+                        _종성 = 종성.indexOf('ㄴ');
+                        새초성 = 초성.indexOf('ㅎ');
+                    } else if (_종성 === 종성.indexOf('ㄺ')) {			// ㄹ / ㄱ
+                        _종성 = 종성.indexOf('ㄹ');
+                        새초성 = 초성.indexOf('ㄱ');
+                    } else if (_종성 === 종성.indexOf('ㄻ')) {			// ㄹ / ㅁ
+                        _종성 = 종성.indexOf('ㄹ');
+                        새초성 = 초성.indexOf('ㅁ');
+                    } else if (_종성 === 종성.indexOf('ㄼ')) {			// ㄹ / ㅂ
+                        _종성 = 종성.indexOf('ㄹ');
+                        새초성 = 초성.indexOf('ㅂ');
+                    } else if (_종성 === 종성.indexOf('ㄽ')) {			// ㄹ / ㅅ
+                        _종성 = 종성.indexOf('ㄹ');
+                        새초성 = 초성.indexOf('ㅅ');
+                    } else if (_종성 === 종성.indexOf('ㄾ')) {			// ㄹ / ㅌ
+                        _종성 = 종성.indexOf('ㄹ');
+                        새초성 = 초성.indexOf('ㅌ');
+                    } else if (_종성 === 종성.indexOf('ㄿ')) {			// ㄹ / ㅍ
+                        _종성 = 종성.indexOf('ㄹ');
+                        새초성 = 초성.indexOf('ㅍ');
+                    } else if (_종성 === 종성.indexOf('ㅀ')) {			// ㄹ / ㅎ
+                        _종성 = 종성.indexOf('ㄹ');
+                        새초성 = 초성.indexOf('ㅎ');
+                    } else if (_종성 === 종성.indexOf('ㅄ')) {			// ㅂ / ㅅ
+                        _종성 = 종성.indexOf('ㅂ');
+                        새초성 = 초성.indexOf('ㅅ');
                     }
-                    else if (_중성 === 중성.indexOf('ㅜ')) {
-                        if (index === 한글.indexOf('ㅓ')) _중성 = 중성.indexOf('ㅝ');                               // 복모음 ㅝ
-                        else if (index === 한글.indexOf('ㅔ')) _중성 = 중성.indexOf('ㅞ');                          // 복모음 ㅞ
-                        else if (index === 한글.indexOf('ㅣ')) _중성 = 중성.indexOf('ㅟ');                          // 복모음 ㅟ
+                    // 복자음 아님 
+                    else {							
+                        새초성 = 초성.indexOf(종성[_종성]);
+                        _종성 = -1;
                     }
-                    else if (_중성 === 중성.indexOf('ㅡ') && index === 한글.indexOf('ㅣ')) _중성 = 중성.indexOf('ㅢ');  // 복모음 ㅢ
-                    // 복모음이 아니라면 이전의 초, 중 조합으로 한글을 생성한다.
-                    else {
+                    // 앞글자가 초성 + 중성 + (종성)
+                    if (_초성 != -1) result += this.한글생성(_초성, _중성, _종성);
+                    // 복자음만 있음
+                    else result += 종성[_종성];
+                    _초성 = 새초성;
+                    _중성 = -1;
+                    _종성 = -1;
+                }
+                if (_중성 === -1) _중성 = 중성.indexOf(_한글);
+                else if (_중성 === 중성.indexOf('ㅗ') && _한글 === 'ㅏ') _중성 = 중성.indexOf('ㅘ');
+                else if (_중성 === 중성.indexOf('ㅗ') && _한글 === 'ㅐ') _중성 = 중성.indexOf('ㅙ');
+                else if (_중성 === 중성.indexOf('ㅗ') && _한글 === 'ㅣ') _중성 = 중성.indexOf('ㅚ');
+                else if (_중성 === 중성.indexOf('ㅜ') && _한글 === 'ㅓ') _중성 = 중성.indexOf('ㅝ');
+                else if (_중성 === 중성.indexOf('ㅜ') && _한글 === 'ㅔ') _중성 = 중성.indexOf('ㅞ');
+                else if (_중성 === 중성.indexOf('ㅜ') && _한글 === 'ㅣ') _중성 = 중성.indexOf('ㅟ');
+                else if (_중성 === 중성.indexOf('ㅡ') && _한글 === 'ㅣ') _중성 = 중성.indexOf('ㅢ');
+                // 조합 안되는 모음
+                else {
+                    // 초성 + 중성 후 중성
+                    if (_초성 != -1) {			
                         result += this.한글생성(_초성, _중성, _종성);
-                        _초성 = -1, _중성 = -1, _종성 = -1;
-                        _중성 = 중성.indexOf(_한글);
-                    }
+                        _초성 = -1;
+                    } 
+                    // 중성 후 중성
+                    else result += 중성[_중성];
+                    _중성 = -1;
+                    result += _한글;
                 }
             }
         }
         // 남아있는 한글 처리
         if (_초성 !== -1) {
-            if (_중성 !== -1) result += this.한글생성(_초성, _중성, _종성);     // 초성 + 중성 + (종성)
-            else result += 초성[_초성]      // 초성만
+            // 초성 + 중성 + (종성)
+            if (_중성 !== -1) result += this.한글생성(_초성, _중성, _종성);     
+            // 초성만
+            else result += 초성[_초성]      
         } else {
-            if (_중성 !== -1) result += 중성[_중성]    // 중성만
-            else if (_종성 !== -1) result += 종성[_종성]    // 종성만 (복자음)
+            // 중성만
+            if (_중성 !== -1) result += 중성[_중성]    
+            // 종성만 (복자음)
+            else if (_종성 !== -1) result += 종성[_종성]    
         }
         return result;
     }
@@ -160,13 +209,13 @@ class Enko {
 }
 
 var enko = new Enko();
-console.log(enko.en2ko('anjgo qnpfrqnpfr'));
+console.log(enko.en2ko('안녕!'));
 console.log(enko.en2ko('qjshs  123'));
 console.log(enko.en2ko('tpdy!'));
 console.log(enko.en2ko('fnffnfkffk'));
 console.log(enko.en2ko('diffkfl'));
 console.log(enko.en2ko('fnffn'));
-console.log(enko.en2ko('zzzzz'));
+console.log(enko.en2ko('qnpfrqnpfr'));
 console.log(enko.en2ko('nnnn'));
 // console.log(한글생성(-1,-1,-1));
 // console.log(is한글("f"));
